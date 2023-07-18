5a74859d59ddab866852b5d2612f5e4af7d971b8
Javier <javi19mono@gmail.com>
2023-07-18 13:31:54 -0500

    Fino

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function ready(){
 }
 //Eliminamos todos los elementos del carrito y lo ocultamos
 function pagarClicked(){
-    alert("Gracias por la compra");
+    // alert("Gracias por la compra");
     //Elimino todos los elmentos del carrito
     var carritoItems = document.getElementsByClassName('carrito-items')[0];
     while (carritoItems.hasChildNodes()){
